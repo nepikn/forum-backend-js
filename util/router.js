d05876fcbab5e3d064791003ce478024951fa1aa
nepikn <31944706+nepikn@users.noreply.github.com>
2024-07-26 22:02:25 +0800

    refactor(router): merge proto assign

--- util/router.js
+++ util/router.js
@@ -1,31 +1,24 @@
 import express from "express";
 
 export default class Router {
-  controller;
-  expressRouter;
-  constructor(controller) {
-    this.controller = controller;
-    this.expressRouter = express.Router();
+  targetRouter = express.Router();
 
-    return new Proxy(this.expressRouter, {
+  setMiddleware(method, path, cb) {
+    this.targetRouter[method](path, cb);
+  }
+
+  constructor(controller) {
+    return new Proxy(this.targetRouter, {
       get: (target, p, receiver) => {
         if (["post", "get", "put", "delete"].includes(p)) {
-          return this[p].bind(this);
+          return (path = "") => {
+            this.setMiddleware(p, path, controller[p]);
+
+            return receiver;
+          };
         }
         return Reflect.get(...arguments);
       },
     });
   }
-
-  setMiddleware(method, path = "") {
-    this.expressRouter[method](path, this.controller[method]);
-  }
-}
-
-for (const method of ["post", "get", "put", "delete"]) {
-  Router.prototype[method] = function (path) {
-    this.setMiddleware(method, path);
-
-    return this;
-  };
 }
