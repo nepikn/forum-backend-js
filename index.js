6b25b983d2ff3a88e8a6efc19c9f5e088d2efde8
nepikn <31944706+nepikn@users.noreply.github.com>
2024-07-31 22:13:29 +0800

    feat: session, cors

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
+import cors from "cors";
 import express from "express";
 import routers from "./routers";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import { config } from "dotenv";
+
+config({
+  path: [`.env.${process.env.NODE_ENV ?? "development"}`, ".env"],
+});
 
 const app = express();
-const port = 3000;
+const port = process.argv[2] ?? 3000;
+const secret = process.env.COOKIE_SECRET;
+
+app.use(cookieParser(secret));
+app.use(session({ secret, resave: false, saveUninitialized: false }));
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGINS.split(",").concat(
+      /^http:\/\/localhost(:\d+)?$/
+    ),
+    credentials: true,
+  })
+);
+// app.use((req, res, next) => {
+//   console.log(req);
+//   next();
+// });
+// console.log(process.env.NODE_ENV);
 
 for (const key of Object.keys(routers)) {
-  app.use(`/${key}`, routers[key]);
+  app.use(`${process.env.API_BASE ?? ""}/${key}`, routers[key]);
 }
 app.use((req, res, next) => {
   if (res.body === undefined) {
